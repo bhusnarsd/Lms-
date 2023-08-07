611e0775581e52b4d37ee2ae7b5c6b6f88d3c59d
lalita mahule <lalita.mahule@simplifedschooling.com>
2023-08-07 14:57:40 +0530

    Medium

--- src/routes/v1/medium.route.js
+++ src/routes/v1/medium.route.js
@@ -17,3 +17,203 @@ router
     .delete(validate(mediumValidation.deleteMedium), mediumController.deleteMedium);
 
 module.exports = router;
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Medium
+ *   description: Medium management and retrieval
+ */
+
+/**
+ * @swagger
+ * /medium:
+ *   post:
+ *     summary: Create a Medium
+ *     description: create other medium.
+ *     tags: [Medium]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - medium               
+ *             properties:
+ *               mediumname:
+ *                 type: string *                              
+ *             example:
+ *               mediumname: English            
+ *                 
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Video'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ *   get:
+ *     summary: Get all Medium
+ *     description: all medium.
+ *     tags: [Medium]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         mediumname: medium
+ *         schema:
+ *           type: string
+ *         description: User name *       
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: sort by query in the form of field:desc/asc (ex. name:asc)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of Medium
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Medium'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /medium/{id}:
+ *   get:
+ *     summary: Get a medium
+ *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
+ *     tags: [Medium]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         mediumname: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Medium id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Medium'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a Medium
+ *     description: Logged in users can only update their own information. Only admins can update other users.
+ *     tags: [Medium]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         mediumname: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: medium id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               mediumname:
+ *                 type: string                
+ *             example:
+ *               mediumname: fake name*               
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Medium'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a Medium
+ *     description: Logged in users can delete only themselves. Only admins can delete other users.
+ *     tags: [Medium]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         mediumname: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: medium id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
